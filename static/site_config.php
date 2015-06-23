57391122d69f844328035ec8b5af761bb9bf2312
Luis Fernando Granda Morales <lfgranda3@utpl.edu.ec>
2015-06-23 12:06:01 -0500

    Nuevos archivos del Sistema de Ventas

---
+++ static/site_config.php
@@ -0,0 +1,16 @@
+<?php
+#DATOS DE LA WEB
+$site_name="Universidad Técnica Particular de Loja";
+$project_name="Joseph Godoy";
+$project_slogan="TVentas";
+$site_autor="@joseph";
+$url_site="http://127.0.0.1/scrum/scrum";
+
+
+#Datos de configuracion de mysql
+$db_host="127.0.0.1";
+$db_user="root";
+$db_password="";
+$db_name="nueva";
+//$db_name="webapp28_4_15db";
+?>
